f28c20cf5d75b5df68ee15abffa469bc8a86eaf6
Ilie Vlad <32937795+vlad434@users.noreply.github.com>
2024-11-28 11:59:04 +0200

    Added auth middleware and logout and logoutAll route handlers

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const User = require("../models/user");
+const auth = require("../middleware/auth");
 const router = new express.Router();
 
 //create users
@@ -33,14 +34,35 @@ router.post("/users/login", async (req, res) => {
   }
 });
 
-//get all users
-router.get("/users", async (req, res) => {
+//logout route
+router.post("/users/logout", auth, async (req, res) => {
   try {
-    const users = await User.find({});
-    res.send(users);
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+
+    await req.user.save();
+
+    res.send();
   } catch (error) {
     res.status(500).send();
   }
+});
+
+//logout all
+router.post("/users/logoutAll", auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+//get all users
+router.get("/users/me", auth, async (req, res) => {
+  res.send(req.user);
 });
 
 //get individual user
