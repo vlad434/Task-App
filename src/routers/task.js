8cb9bc108637112729bd3395d40d1d08674f3ba2
Ilie Vlad <32937795+vlad434@users.noreply.github.com>
2024-12-02 11:48:12 +0200

    Added timestamps, filtering, pagination and sorting

--- src/routers/task.js
+++ src/routers/task.js
@@ -19,9 +19,29 @@ router.post("/tasks", auth, async (req, res) => {
 
 //get all tasks
 router.get("/tasks", auth, async (req, res) => {
+  const match = {};
+  const sort = {};
+
+  if (req.query.completed) {
+    match.completed = req.query.completed === "true";
+  }
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(":");
+    sort[parts[0]] = parts[1] === "desc" ? -1 : 1; //asc = 1, desc = -1
+  }
+
   try {
     // const tasks = await Task.findById({ owner: req.user._id });
-    await req.user.populate("tasks");
+    await req.user.populate({
+      path: "tasks",
+      match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+        sort,
+      },
+    });
     res.send(req.user.tasks);
   } catch (error) {
     res.status(500).send(error);
